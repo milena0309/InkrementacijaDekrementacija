df83a9710567e85a4ef351b35818943c50f6208e
Milena <Milena@DESKTOP-CP344GO>
2019-02-09 22:00:24 +0100

    IT 09.02.2019.

---
+++ src/inkrementacijadekrementacija/InkrementacijaDekrementacija.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inkrementacijadekrementacija;
+
+import java.awt.BorderLayout;
+
+/**
+ *
+ * @author Milena
+ */
+public class InkrementacijaDekrementacija {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // 
+        
+        int i = 1;
+        // prva vrednost za i
+        System.out.println("i = " + i);
+        System.out.println("i++ = " + (i++));
+        System.out.println("i = " + i);
+        
+        // druga vrednost za p
+        int p = 1;
+        System.out.println("p = " + p);
+        System.out.println("++p = " + (++p));
+        System.out.println("p = " + p);
+        
+        
+    }
+    
+}
